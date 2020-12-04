680de6a72c85138e3a9fcd6d864abc5166a7078f
Randy Schott <1815175+schottra@users.noreply.github.com>
2020-12-04 12:15:43 -0800

    fix: upgrade TS to fix error and cleanup resulting errors

--- src/common/env.ts
+++ src/common/env.ts
@@ -1,8 +1,5 @@
 import { Env } from 'config';
-import { cloneDeep } from 'lodash';
-
-const global = process != null ? process : window;
 
 /** equivalent to process.env in server and client */
 // tslint:disable-next-line:no-any
-export const env: Env = cloneDeep(global.env) as any;
+export const env: Env = Object.assign({}, process.env, window.env);
