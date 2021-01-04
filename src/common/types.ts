7ea4189b1c80d907ee6dbbdfa0c107def1a9f0d7
Randy Schott <1815175+schottra@users.noreply.github.com>
2021-01-04 11:40:47 -0800

    fix: show DataError by default for failed queries

--- src/common/types.ts
+++ src/common/types.ts
@@ -13,6 +13,9 @@ export interface ResourceDefinition {
     type: string;
 }
 
+/** Converts type `T` to one in which all fields are optional and
+ * all fields in nested objects are also optional.
+ */
 export type DeepPartial<T> = {
     [P in keyof T]?: DeepPartial<T[P]>;
 };
