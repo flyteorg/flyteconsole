74319756aa02008bf41562f7404020c8e235e571
Pianist038801 <steven@union.ai>
2021-08-23 06:15:00 -0400

    feat: schema validator for struct type execution
    Signed-off-by: Pianist038801 <steven@union.ai>

--- src/components/Launch/LaunchForm/StructInput.tsx
+++ src/components/Launch/LaunchForm/StructInput.tsx
@@ -1,8 +1,20 @@
-import { TextField } from '@material-ui/core';
+import { TextField, Card, CardContent, CardHeader } from '@material-ui/core';
 import * as React from 'react';
+import { useState } from 'react';
+import Form from '@rjsf/material-ui';
+import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles';
 import { makeStringChangeHandler } from './handlers';
 import { InputProps } from './types';
 import { getLaunchInputId } from './utils';
+import { protobufValueToPrimitive, PrimitiveType } from './inputHelpers/struct';
+
+const muiTheme = createMuiTheme({
+    props: {
+        MuiTextField: {
+            variant: 'outlined'
+        }
+    }
+});
 
 /** Handles rendering of the input component for a Struct */
 export const StructInput: React.FC<InputProps> = props => {
@@ -11,12 +23,47 @@ export const StructInput: React.FC<InputProps> = props => {
         label,
         name,
         onChange,
-        typeDefinition: { subtype },
+        typeDefinition: { literalType },
         value = ''
     } = props;
     const hasError = !!error;
     const helperText = hasError ? error : props.helperText;
-    return (
+
+    const [paramData, setParamData] = useState({});
+    const onFormChange = ({ formData }, e) => {
+        onChange(JSON.stringify(formData));
+        setParamData(formData);
+    };
+
+    let jsonFormRenderable = false;
+    let parsedJson: PrimitiveType = {};
+
+    if (literalType?.metadata) {
+        parsedJson = protobufValueToPrimitive(
+            literalType.metadata.fields.definitions
+        );
+        if (parsedJson.DatumSchema) jsonFormRenderable = true;
+    }
+
+    return jsonFormRenderable ? (
+        <MuiThemeProvider theme={muiTheme}>
+            <Card>
+                <CardHeader
+                    title={label}
+                    style={{ borderBottom: 'solid 1px gray' }}
+                />
+                <CardContent>
+                    <Form
+                        schema={parsedJson.DatumSchema}
+                        formData={paramData}
+                        onChange={onFormChange}
+                    >
+                        <div></div>
+                    </Form>
+                </CardContent>
+            </Card>
+        </MuiThemeProvider>
+    ) : (
         <TextField
             id={getLaunchInputId(name)}
             error={hasError}
