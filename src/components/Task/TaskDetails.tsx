a2dacd829b4d7ee7070ee741d07f398cdb95fa67
vinayak017 <72243768+vinayak017@users.noreply.github.com>
2021-10-12 20:03:30 +0530

    Update TaskDetails.tsx

--- src/components/Task/TaskDetails.tsx
+++ src/components/Task/TaskDetails.tsx
@@ -2,15 +2,12 @@ import { withRouteParams } from 'components/common/withRouteParams';
 import { EntityDetails } from 'components/Entities/EntityDetails';
 import { ResourceIdentifier, ResourceType } from 'models/Common/types';
 import * as React from 'react';
-
 export interface TaskDetailsRouteParams {
     projectId: string;
     domainId: string;
     taskName: string;
 }
 export type TaskDetailsProps = TaskDetailsRouteParams;
-
-/** The view component for the Task landing page */
 export const TaskDetailsContainer: React.FC<TaskDetailsRouteParams> = ({
     projectId,
     domainId,
@@ -27,7 +24,6 @@ export const TaskDetailsContainer: React.FC<TaskDetailsRouteParams> = ({
     );
     return <EntityDetails id={id} />;
 };
-
 export const TaskDetails = withRouteParams<TaskDetailsRouteParams>(
     TaskDetailsContainer
 );
