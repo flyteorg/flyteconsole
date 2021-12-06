5fa0bb060455df4be22105ffccafc978bd38141b
csirius <85753828+csirius@users.noreply.github.com>
2021-12-06 14:40:32 -0500

    fix: unit test failure
    Signed-off-by: csirius <85753828+csirius@users.noreply.github.com>

--- src/components/Workflow/useWorkflowInfoList.ts
+++ src/components/Workflow/useWorkflowInfoList.ts
@@ -1,13 +1,15 @@
 import { DomainIdentifierScope, ResourceType } from 'models/Common/types';
 import { RequestConfig } from 'models/AdminEntity/types';
 import { usePagination } from 'components/hooks/usePagination';
-import { listNamedEntities } from 'models/Common/api';
 import { WorkflowListStructureItem } from './types';
+import { useAPIContext } from 'components/data/apiContext';
 
 export const useWorkflowInfoList = (
     scope: DomainIdentifierScope,
     config?: RequestConfig
 ) => {
+    const { listNamedEntities } = useAPIContext();
+
     return usePagination<WorkflowListStructureItem, DomainIdentifierScope>(
         { ...config, fetchArg: scope },
         async (scope, requestConfig) => {
