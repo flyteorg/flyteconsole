62ff59156e93f934acac1e499029afd798cc566a
Nastya Rusina <nastya@union.ai>
2022-03-15 14:08:21 -0700

    chore: prettier run through p.7
    Signed-off-by: Nastya Rusina <nastya@union.ai>

--- .storybook/main.js
+++ .storybook/main.js
@@ -3,7 +3,11 @@ module.exports = {
     builder: 'webpack5',
   },
   stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
-  addons: ['@storybook/addon-links', '@storybook/addon-essentials', '@storybook/addon-interactions'],
+  addons: [
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    '@storybook/addon-interactions',
+  ],
   framework: '@storybook/react',
   webpackFinal: async (config, { configType }) => {
     config.resolve.modules = ['src', 'node_modules'];
