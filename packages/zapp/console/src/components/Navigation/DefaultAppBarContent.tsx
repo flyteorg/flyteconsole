31e63c89f9dc74552172060d750e0b488b925907
Nastya <55718143+anrusina@users.noreply.github.com>
2022-05-11 13:36:47 -0700

    chore: Provide basic repo structure and ensure that stories, tests, vscode setup properly (#458)
    * chore: separate AppInfo
    * chore: vscode properly understands @flyteconsole
    * chore: add basics/locale entry
    * test: add storybooks and tests
    * test: fix istanbul test-coverage issue by binding babel versions
    
    Signed-off-by: Nastya Rusina <nastya@union.ai>

--- packages/zapp/console/src/components/Navigation/DefaultAppBarContent.tsx
+++ packages/zapp/console/src/components/Navigation/DefaultAppBarContent.tsx
@@ -1,11 +1,17 @@
 import { makeStyles, Theme } from '@material-ui/core/styles';
 import classnames from 'classnames';
-import { FlyteLogo } from 'components/common/FlyteLogo';
+import { AppInfo, VersionInfo } from '@flyteconsole/components';
+import { FlyteLogo } from '@flyteconsole/ui-atoms';
 import { useCommonStyles } from 'components/common/styles';
 import * as React from 'react';
 import { Link } from 'react-router-dom';
 import { Routes } from 'routes/routes';
+import { useAdminVersion } from 'components/hooks/useVersion';
+import { env } from 'common/env';
 import { UserInformation } from './UserInformation';
+import t, { patternKey } from './strings';
+
+const { version: platformVersion } = require('../../../package.json');
 
 const useStyles = makeStyles((theme: Theme) => ({
   spacer: {
@@ -20,13 +26,34 @@ const useStyles = makeStyles((theme: Theme) => ({
 export const DefaultAppBarContent: React.FC = () => {
   const commonStyles = useCommonStyles();
   const styles = useStyles();
+  const { adminVersion } = useAdminVersion();
+
+  const versions: VersionInfo[] = [
+    {
+      name: t('versionConsoleUi'),
+      version: platformVersion,
+      url: `https://github.com/flyteorg/flyteconsole/releases/tag/v${platformVersion}`,
+    },
+    {
+      name: t('versionAdmin'),
+      version: adminVersion,
+      url: `https://github.com/flyteorg/flyteadmin/releases/tag/v${adminVersion}`,
+    },
+    {
+      name: t('versionGoogleAnalytics'),
+      version: t(patternKey('gaDisable', env.DISABLE_GA)),
+      url: 'https://github.com/flyteorg/flyteconsole#google-analytics',
+    },
+  ];
+
   return (
     <>
       <Link className={classnames(commonStyles.linkUnstyled)} to={Routes.SelectProject.path}>
-        <FlyteLogo size={32} variant="dark" />
+        <FlyteLogo size={32} />
       </Link>
       <div className={styles.spacer} />
       <UserInformation />
+      <AppInfo versions={versions} documentationUrl="https://docs.flyte.org/en/latest/" />
     </>
   );
 };
