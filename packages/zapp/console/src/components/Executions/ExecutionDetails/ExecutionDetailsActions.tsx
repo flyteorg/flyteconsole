d38b98b2d68700415224ad6c621d79e928fded01
Carina Ursu <carinaelena@gmail.com>
2022-06-10 10:36:21 -0700

    fix: cannot read properties of undefined (#506)
    fix: nullref
    
    Signed-off-by: Carina Ursu <carina@union.ai>
    
    Co-authored-by: Carina Ursu <carina@union.ai>

--- packages/zapp/console/src/components/Executions/ExecutionDetails/ExecutionDetailsActions.tsx
+++ packages/zapp/console/src/components/Executions/ExecutionDetails/ExecutionDetailsActions.tsx
@@ -1,6 +1,6 @@
 import { Button } from '@material-ui/core';
 import * as React from 'react';
-import { ResourceIdentifier, Identifier, Variable } from 'models/Common/types';
+import { ResourceIdentifier, Identifier } from 'models/Common/types';
 import { getTask } from 'models/Task/api';
 import { LaunchFormDialog } from 'components/Launch/LaunchForm/LaunchFormDialog';
 import { NodeExecutionIdentifier } from 'models/Execution/types';
@@ -20,32 +20,37 @@ export const ExecutionDetailsActions = (props: ExecutionDetailsActionsProps): JS
   const { className, details, nodeExecutionId } = props;
 
   const [showLaunchForm, setShowLaunchForm] = React.useState<boolean>(false);
-  const [taskInputsTypes, setTaskInputsTypes] = React.useState<
-    Record<string, Variable> | undefined
-  >();
+
+  const [initialParameters, setInitialParameters] = React.useState<
+    TaskInitialLaunchParameters | undefined
+  >(undefined);
 
   const executionData = useNodeExecutionData(nodeExecutionId);
-
-  const id = details.taskTemplate?.id as ResourceIdentifier | undefined;
+  const id = details.taskTemplate?.id;
 
   React.useEffect(() => {
-    const fetchTask = async () => {
-      const task = await getTask(id as Identifier);
-      setTaskInputsTypes(task.closure.compiledTask.template?.interface?.inputs?.variables);
-    };
-    if (id) fetchTask();
-  }, [id]);
+    if (!id) {
+      return;
+    }
 
-  if (!id) {
+    (async () => {
+      const task = await getTask(id!);
+
+      const literals = executionData.value.fullInputs?.literals;
+      const taskInputsTypes = task.closure.compiledTask.template?.interface?.inputs?.variables;
+
+      const tempInitialParameters: TaskInitialLaunchParameters = {
+        values: literals && taskInputsTypes && literalsToLiteralValueMap(literals, taskInputsTypes),
+        taskId: id as Identifier | undefined,
+      };
+
+      setInitialParameters(tempInitialParameters);
+    })();
+  }, [details]);
+
+  if (!id || !initialParameters) {
     return <></>;
   }
-
-  const literals = executionData.value.fullInputs?.literals;
-
-  const initialParameters: TaskInitialLaunchParameters = {
-    values: literals && taskInputsTypes && literalsToLiteralValueMap(literals, taskInputsTypes),
-    taskId: id as Identifier | undefined,
-  };
 
   const rerunOnClick = (e: React.MouseEvent<HTMLElement>) => {
     e.stopPropagation();
@@ -60,7 +65,7 @@ export const ExecutionDetailsActions = (props: ExecutionDetailsActionsProps): JS
         </Button>
       </div>
       <LaunchFormDialog
-        id={id}
+        id={id as ResourceIdentifier}
         initialParameters={initialParameters}
         showLaunchForm={showLaunchForm}
         setShowLaunchForm={setShowLaunchForm}
