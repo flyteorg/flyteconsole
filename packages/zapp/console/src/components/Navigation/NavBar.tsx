6127fc201839111a1151d9825206a55ed35b106e
Nastya <55718143+anrusina@users.noreply.github.com>
2022-06-15 12:08:35 -0700

    feat: navbar navigstion dropdown
    Signed-off-by: Nastya <55718143+anrusina@users.noreply.github.com>

--- packages/zapp/console/src/components/Navigation/NavBar.tsx
+++ packages/zapp/console/src/components/Navigation/NavBar.tsx
@@ -3,15 +3,26 @@ import Toolbar from '@material-ui/core/Toolbar';
 import { navBarContentId } from 'common/constants';
 import * as React from 'react';
 import { DefaultAppBarContent } from './DefaultAppBarContent';
+import { FlyteNavData } from './utils';
 
 /** Contains all content in the top navbar of the application. */
 export const NavBar: React.FC<{ useCustomContent?: boolean }> = ({
   /** Allow injection of custom content. */
   useCustomContent = false,
 }) => {
-  const content = useCustomContent ? <div id={navBarContentId} /> : <DefaultAppBarContent />;
+  const content = useCustomContent ? (
+    <div id={navBarContentId} />
+  ) : (
+    <DefaultAppBarContent items={FlyteNavData?.items ?? []} />
+  );
   return (
-    <AppBar color="secondary" elevation={0} id="navbar" position="fixed">
+    <AppBar
+      color="secondary"
+      elevation={0}
+      id="navbar"
+      position="fixed"
+      style={{ color: FlyteNavData?.color, background: FlyteNavData?.background }}
+    >
       <Toolbar id={navBarContentId}>{content}</Toolbar>
     </AppBar>
   );
