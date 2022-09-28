c41dd8561eacd213f24b11a2f693f1d57a3e60eb
Carina Ursu <carina@union.ai>
2022-09-27 17:34:23 -0700

    chore: add test
    Signed-off-by: Carina Ursu <carina@union.ai>

---
+++ packages/plugins/components/src/NavigationDropdown/NavigationDropdown.test.tsx
@@ -0,0 +1,47 @@
+import { ThemeProvider } from '@material-ui/styles';
+import * as React from 'react';
+import { render, within, fireEvent, waitFor } from '@testing-library/react';
+
+import { createMuiTheme } from '@material-ui/core';
+import { NavigationDropdown } from '.';
+
+const menuItems = [
+  {
+    title: 'Projects',
+    url: '/console',
+  },
+  {
+    title: 'Org',
+    url: '/dashboard',
+  },
+];
+const muiTheme = createMuiTheme({});
+
+describe('NavigationDopdown', () => {
+  it('dropdown opens on click', async () => {
+    const { getByRole } = render(
+      <ThemeProvider theme={muiTheme}>
+        <NavigationDropdown
+          baseUrl="/console"
+          items={menuItems}
+          config={{ headerFontFamily: `"Open Sans", helvetica, arial, sans-serif` }}
+        />
+      </ThemeProvider>,
+    );
+
+    const defaultButton = getByRole('button');
+    expect(defaultButton).not.toBeNull();
+
+    expect(defaultButton.textContent).toEqual(menuItems[0].title);
+
+    fireEvent.mouseDown(defaultButton);
+    const listbox = within(getByRole('listbox'));
+    await waitFor(() => {
+      expect(listbox.getAllByRole('option').length).toEqual(2);
+    });
+
+    await waitFor(() => {
+      expect(listbox.getByText(menuItems[1].title)).toBeInTheDocument();
+    });
+  });
+});
--- packages/plugins/components/src/NavigationDropdown/NavigationDropdown.tsx
+++ packages/plugins/components/src/NavigationDropdown/NavigationDropdown.tsx
@@ -2,8 +2,8 @@ import * as React from 'react';
 import { makeStyles, Theme } from '@material-ui/core/styles';
 import { MenuItem, Select, SelectProps, StyledComponentProps } from '@material-ui/core';
 import { useHistory } from 'react-router-dom';
-import { getBasePathName, makeRoute } from '@flyteconsole/components';
 import classNames from 'classnames';
+import { getBasePathName, makeRoute } from '../Utils';
 import { FlyteNavItem } from './types';
 
 const DEFAULT_SELECT_LABELID = 'demo-controlled-open-select-label';
@@ -77,6 +77,7 @@ export const NavigationDropdown = (props: NavigationDropdownProps) => {
 
   return (
     <Select
+      data-testid={DEFAULT_SELECT_ID}
       labelId={DEFAULT_SELECT_LABELID}
       id={DEFAULT_SELECT_ID}
       open={open}
