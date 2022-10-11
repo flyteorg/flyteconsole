8550e88633b9dda8be299d954d87abee7775a151
Carina Ursu <carina@union.ai>
2022-10-10 18:43:07 -0700

    chore: default app
    Signed-off-by: Carina Ursu <carina@union.ai>

--- packages/zapp/console/src/components/Navigation/NavBar.tsx
+++ packages/zapp/console/src/components/Navigation/NavBar.tsx
@@ -2,23 +2,59 @@ import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import { navBarContentId } from 'common/constants';
 import * as React from 'react';
-import { FlyteNavigation } from '@flyteconsole/components';
-import { DefaultAppBarContent } from './DefaultAppBarContent';
+import { env, FlyteNavigation, useAdminVersion, VersionInfo, DefaultAppBarContent } from '@flyteconsole/components';
+import { Routes } from 'routes/routes';
 import { getFlyteNavigationData } from './utils';
+import t, { patternKey } from './strings';
+
+const { version: platformVersion } = require('../../../package.json');
 
 export interface NavBarProps {
   useCustomContent?: boolean;
   navigationData?: FlyteNavigation;
 }
 
-/** Contains all content in the top navbar of the application. */
-export const NavBar = (props: NavBarProps) => {
-  const navData = props.navigationData ?? getFlyteNavigationData();
+export interface DefaultAppBarContentWrapperProps extends NavBarProps {
+
+}
+export const DefaultAppBarContentWrapper = (props: DefaultAppBarContentWrapperProps) => {
+  const navData = props.navigationData;
+  const { adminVersion } = useAdminVersion();
+
+  const versions: VersionInfo[] = React.useMemo(
+    () => [
+      {
+        name: t('versionConsoleUi'),
+        version: platformVersion,
+        url: `https://github.com/flyteorg/flyteconsole/releases/tag/v${platformVersion}`,
+      },
+      {
+        name: t('versionAdmin'),
+        version: adminVersion,
+        url: `https://github.com/flyteorg/flyteadmin/releases/tag/v${adminVersion}`,
+      },
+      {
+        name: t('versionGoogleAnalytics'),
+        version: t(patternKey('gaDisable', env.DISABLE_GA)),
+        url: 'https://github.com/flyteorg/flyteconsole#google-analytics',
+      },
+    ],
+    [],
+  );
+
   const content = props.useCustomContent ? (
     <div id={navBarContentId} />
   ) : (
-    <DefaultAppBarContent items={navData?.items ?? []} console={navData?.console} />
+    <DefaultAppBarContent routes={Routes} versions={versions} items={navData?.items ?? []} console={navData?.console} />
   );
+
+  return content;
+}
+
+
+/** Contains all content in the top navbar of the application. */
+export const NavBar = (props: NavBarProps) => {
+  const navData = props.navigationData ?? getFlyteNavigationData();
 
   return (
     <AppBar
@@ -28,7 +64,7 @@ export const NavBar = (props: NavBarProps) => {
       position="fixed"
       style={{ color: navData?.color, background: navData?.background }}
     >
-      <Toolbar id={navBarContentId}>{content}</Toolbar>
+      <Toolbar id={navBarContentId}><DefaultAppBarContentWrapper {...props} /></Toolbar>
     </AppBar>
   );
 };
