c36e80a4e312c2c0ecfb798046405c3b7fbc51bf
Carina Ursu <carina@union.ai>
2023-03-09 11:54:48 -0800

    chore: show correct app version in info  (#716)
    * chore: show correct app version in info
    
    Signed-off-by: Carina Ursu <carina@union.ai>
    
    * chore: update ver
    
    Signed-off-by: Carina Ursu <carina@union.ai>
    
    ---------
    
    Signed-off-by: Carina Ursu <carina@union.ai>
    Signed-off-by: 4nalog <4nalog@protonmail.com>

--- packages/console/src/components/Navigation/NavBar.tsx
+++ packages/console/src/components/Navigation/NavBar.tsx
@@ -1,11 +1,9 @@
+import * as React from 'react';
+import { Suspense, lazy } from 'react';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import { navBarContentId } from 'common/constants';
-import { COLOR_SPECTRUM } from 'components/Theme/colorSpectrum';
-import { whiteColor } from 'components/Theme/constants';
 import { FlyteNavigation } from '@flyteorg/common';
-import * as React from 'react';
-import { DefaultAppBarContent } from './DefaultAppBarContent';
 import { getFlyteNavigationData } from './utils';
 
 export interface NavBarProps {
@@ -13,16 +11,20 @@ export interface NavBarProps {
   navigationData?: FlyteNavigation;
 }
 
+const DefaultAppBarContent = lazy(() => import('./DefaultAppBarContent'));
+
 /** Contains all content in the top navbar of the application. */
 export const NavBar = (props: NavBarProps) => {
   const navData = props.navigationData ?? getFlyteNavigationData();
   const content = props.useCustomContent ? (
     <div id={navBarContentId} />
   ) : (
-    <DefaultAppBarContent
-      items={navData?.items ?? []}
-      console={navData?.console}
-    />
+    <Suspense fallback={null}>
+      <DefaultAppBarContent
+        items={navData?.items ?? []}
+        console={navData?.console}
+      />
+    </Suspense>
   );
 
   return (
@@ -40,3 +42,5 @@ export const NavBar = (props: NavBarProps) => {
     </AppBar>
   );
 };
+
+export default NavBar;
