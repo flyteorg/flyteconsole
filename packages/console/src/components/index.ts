e4f12de7bc02d2e388704fcd86546162a9b90ef3
Carina Ursu <carina@union.ai>
2023-03-24 10:40:41 -0700

    chore: add build:watch to all packages
    Signed-off-by: Carina Ursu <carina@union.ai>

--- packages/console/src/components/index.ts
+++ packages/console/src/components/index.ts
@@ -1,21 +1,7 @@
-export * from './Navigation';
 export * from './App/App';
+export * from './Navigation';
 
-export { NavBarContent } from './Navigation/NavBarContent';
-export { useNodeExecution } from './hooks/useNodeExecution';
-export { useConditionalQuery } from './hooks/useConditionalQuery';
+export * from './Executions';
 
-export * from './Executions/types';
-export { type ExecutionDetailsRouteParams } from './Executions/ExecutionDetails/ExecutionDetails';
-export { ExecutionStatusBadge } from './Executions/ExecutionStatusBadge';
-export { formatRetryAttempt } from './Executions/TaskExecutionsList/utils';
-export { TaskExecutionDetails } from './Executions/TaskExecutionsList/TaskExecutionDetails';
-export { makeTaskExecutionListQuery } from './Executions/taskExecutionQueries';
-export { useWorkflowExecutionQuery } from './Executions/useWorkflowExecution';
-export { NodeExecutionDetailsContextProvider } from './Executions/contextProvider/NodeExecutionDetails';
-export { getNodeFrontendPhase } from './Executions/utils';
-export { useWorkflowExecutionData } from './Executions/useWorkflowExecution';
-export { useNodeExecutionContext } from './Executions/contextProvider/NodeExecutionDetails';
-export { ExecutionDetailsActions } from './Executions/ExecutionDetails/ExecutionDetailsActions';
-export { makeNodeExecutionQuery } from './Executions/nodeExecutionQueries';
+export * from './hooks';
 export * from './common';
