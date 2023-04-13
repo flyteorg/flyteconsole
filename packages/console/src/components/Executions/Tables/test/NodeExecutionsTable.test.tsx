75f87b34ec0af30a92b0a218e0097b569f1fa6c6
Carina Ursu <carina@union.ai>
2023-04-13 11:52:34 -0700

    chore: more tests
    Signed-off-by: Carina Ursu <carina@union.ai>

--- packages/console/src/components/Executions/Tables/test/NodeExecutionsTable.test.tsx
+++ packages/console/src/components/Executions/Tables/test/NodeExecutionsTable.test.tsx
@@ -1,6 +1,11 @@
 import { render, waitFor } from '@testing-library/react';
-import { NodeExecutionDetailsContextProvider } from 'components/Executions/contextProvider/NodeExecutionDetails';
-import { WorkflowNodeExecutionsContext } from 'components/Executions/contexts';
+import {
+  NodeExecutionDetailsContextProvider,
+} from 'components/Executions/contextProvider/NodeExecutionDetails';
+import {
+  ExecutionContext,
+  WorkflowNodeExecutionsContext,
+} from 'components/Executions/contexts';
 import { basicPythonWorkflow } from 'mocks/data/fixtures/basicPythonWorkflow';
 import { noExecutionsFoundString } from 'common/constants';
 import { mockWorkflowId } from 'mocks/data/fixtures/types';
@@ -13,6 +18,8 @@ import { QueryClient, QueryClientProvider } from 'react-query';
 import { createTestQueryClient } from 'test/utils';
 import { dNode } from 'models/Graph/types';
 import { useNodeExecutionFiltersState } from 'components/Executions/filters/useExecutionFiltersState';
+import { Execution, NodeExecution } from 'models';
+import { listNodeExecutions, listTaskExecutions } from 'models/Execution/api';
 import { NodeExecutionsTable } from '../NodeExecutionsTable';
 
 jest.mock('components/Workflow/workflowQueries');
@@ -27,14 +34,21 @@ mockUseNodeExecutionFiltersState.mockReturnValue({
 });
 
 jest.mock('components/Executions/Tables/NodeExecutionRow', () => ({
-  NodeExecutionRow: jest.fn(({ nodeExecution }) => (
+  NodeExecutionRow: jest.fn(({ node }) => (
     <div data-testid="node-execution-row">
-      <div data-testid="node-execution-col-id">{nodeExecution?.id?.nodeId}</div>
+      <div data-testid="node-execution-col-id">
+        {node?.execution?.id?.nodeId}
+      </div>
       <div data-testid="node-execution-col-phase">
-        {nodeExecution?.closure?.phase}
+        {node?.execution?.closure?.phase}
       </div>
     </div>
   )),
+}));
+
+jest.mock('models/Execution/api', () => ({
+  listNodeExecutions: jest.fn(),
+  listTaskExecutions: jest.fn(),
 }));
 
 const mockNodes = (n: number): dNode[] => {
@@ -51,6 +65,7 @@ const mockNodes = (n: number): dNode[] => {
   }
   return nodes;
 };
+const executionId = { domain: 'domain', name: 'name', project: 'project' };
 
 const mockExecutionsById = (n: number, phases: NodeExecutionPhase[]) => {
   const nodeExecutionsById = {};
@@ -63,7 +78,7 @@ const mockExecutionsById = (n: number, phases: NodeExecutionPhase[]) => {
         phase: phases[i - 1],
       },
       id: {
-        executionId: { domain: 'domain', name: 'name', project: 'project' },
+        executionId,
         nodeId: `node${i}`,
       },
       inputUri: '',
@@ -85,24 +100,47 @@ describe('NodeExecutionsTableExecutions > Tables > NodeExecutionsTable', () => {
     fetchWorkflow.mockImplementation(() =>
       Promise.resolve(fixture.workflows.top),
     );
+
+    listNodeExecutions.mockImplementation(() => {
+      return Promise.resolve({
+        entities: Object.values([]),
+      });
+    });
+    listTaskExecutions.mockImplementation(() => {
+      return Promise.resolve({
+        entities: Object.values([]),
+      });
+    });
   });
 
-  const renderTable = ({ nodeExecutionsById, initialNodes, filteredNodes }) =>
+  const renderTable = ({ nodeExecutionsById, initialNodes }) =>
     render(
       <QueryClientProvider client={queryClient}>
-        <NodeExecutionDetailsContextProvider workflowId={mockWorkflowId}>
-          <WorkflowNodeExecutionsContext.Provider
-            value={{
-              nodeExecutionsById,
-              setCurrentNodeExecutionsById: () => {},
-            }}
-          >
-            <NodeExecutionsTable
-              initialNodes={initialNodes}
-              filteredNodes={filteredNodes}
-            />
-          </WorkflowNodeExecutionsContext.Provider>
-        </NodeExecutionDetailsContextProvider>
+        <ExecutionContext.Provider
+          value={{
+            execution: {
+              id: executionId,
+            } as Execution,
+          }}
+        >
+          <NodeExecutionDetailsContextProvider workflowId={mockWorkflowId}>
+            <WorkflowNodeExecutionsContext.Provider
+              value={{
+                dagData: {
+                  dagError: null,
+                  mergedDag: {},
+                },
+                initialDNodes: initialNodes,
+                nodeExecutionsById,
+                setCurrentNodeExecutionsById: () => {},
+                setShouldUpdate: () => {},
+                shouldUpdate: false,
+              }}
+            >
+              <NodeExecutionsTable />
+            </WorkflowNodeExecutionsContext.Provider>
+          </NodeExecutionDetailsContextProvider>
+        </ExecutionContext.Provider>
       </QueryClientProvider>,
     );
 
@@ -110,7 +148,6 @@ describe('NodeExecutionsTableExecutions > Tables > NodeExecutionsTable', () => {
     const { queryByText, queryByTestId } = renderTable({
       initialNodes: [],
       nodeExecutionsById: {},
-      filteredNodes: [],
     });
 
     await waitFor(() => queryByText(noExecutionsFoundString));
@@ -126,14 +163,13 @@ describe('NodeExecutionsTableExecutions > Tables > NodeExecutionsTable', () => {
     const { queryAllByTestId } = renderTable({
       initialNodes,
       nodeExecutionsById,
-      filteredNodes: undefined,
     });
 
-    await waitFor(() => queryAllByTestId('node-execution-row'));
-
-    expect(queryAllByTestId('node-execution-row')).toHaveLength(
-      initialNodes.length,
-    );
+    await waitFor(() => {
+      const nodes = queryAllByTestId('node-execution-row');
+      expect(nodes).toHaveLength(initialNodes.length);
+      return nodes;
+    });
     const ids = queryAllByTestId('node-execution-col-id');
     expect(ids).toHaveLength(initialNodes.length);
     const renderedPhases = queryAllByTestId('node-execution-col-phase');
@@ -147,14 +183,26 @@ describe('NodeExecutionsTableExecutions > Tables > NodeExecutionsTable', () => {
   it('renders NodeExecutionRows with initialNodes even when filterNodes were provided, if appliedFilters is empty', async () => {
     const phases = [NodeExecutionPhase.FAILED, NodeExecutionPhase.SUCCEEDED];
     const nodeExecutionsById = mockExecutionsById(2, phases);
-    const filteredNodes = mockNodes(1);
+    const filteredNodeExecutions = nodeExecutionsById['n1'];
+    listNodeExecutions.mockImplementation(() => {
+      return Promise.resolve({
+        entities: filteredNodeExecutions,
+      });
+    });
 
     const { queryAllByTestId } = renderTable({
       initialNodes,
       nodeExecutionsById,
-      filteredNodes,
     });
 
+    await waitFor(() =>
+      expect(listNodeExecutions).toHaveBeenCalledWith(
+        expect.objectContaining(executionId),
+        expect.objectContaining({
+          filter: [],
+        }),
+      ),
+    );
     await waitFor(() => queryAllByTestId('node-execution-row'));
 
     expect(queryAllByTestId('node-execution-row')).toHaveLength(
@@ -170,35 +218,107 @@ describe('NodeExecutionsTableExecutions > Tables > NodeExecutionsTable', () => {
     }
   });
 
-  it('renders NodeExecutionRows with filterNodes if appliedFilters is not empty', async () => {
-    mockUseNodeExecutionFiltersState.mockReturnValueOnce({
+  it('renders NodeExecutionRows with filterNodes if appliedFilters are less than original filters', async () => {
+    const appliedFilters = [
+      { key: 'phase', operation: 'value_in', value: ['FAILED'] },
+    ];
+    mockUseNodeExecutionFiltersState.mockReturnValue({
       filters: [],
-      appliedFilters: [
-        { key: 'phase', operation: 'value_in', value: ['FAILED', 'SUCCEEDED'] },
-      ],
+      appliedFilters,
     });
 
     const phases = [NodeExecutionPhase.FAILED, NodeExecutionPhase.SUCCEEDED];
     const nodeExecutionsById = mockExecutionsById(2, phases);
-    const filteredNodes = mockNodes(1);
+    const filteredNodeExecutions = [nodeExecutionsById['n1']];
+    listNodeExecutions.mockImplementation(() => {
+      return Promise.resolve({
+        entities: filteredNodeExecutions,
+      });
+    });
+
+    const { queryAllByTestId, debug, container } = renderTable({
+      initialNodes,
+      nodeExecutionsById,
+    });
+
+    await waitFor(() =>
+      expect(listNodeExecutions).toHaveBeenCalledWith(
+        expect.objectContaining(executionId),
+        expect.objectContaining({
+          filter: appliedFilters,
+        }),
+      ),
+    );
+
+    await waitFor(() => {
+      const rows = queryAllByTestId('node-execution-row');
+      return rows.length === filteredNodeExecutions.length;
+    });
+
+    expect(queryAllByTestId('node-execution-row')).toHaveLength(
+      filteredNodeExecutions.length,
+    );
+
+    const ids = queryAllByTestId('node-execution-col-id');
+    expect(ids).toHaveLength(filteredNodeExecutions.length);
+    const renderedPhases = queryAllByTestId('node-execution-col-phase');
+    expect(renderedPhases).toHaveLength(filteredNodeExecutions.length);
+    debug(container)
+
+    for (const i in filteredNodeExecutions) {
+      expect(ids[i]).toHaveTextContent(filteredNodeExecutions[i].id?.nodeId);
+      expect(renderedPhases[i]).toHaveTextContent(phases[i].toString());
+    }
+  });
+
+  it('renders NodeExecutionRows with filterNodes if appliedFilters are the same as original filters', async () => {
+    const phases = [NodeExecutionPhase.FAILED, NodeExecutionPhase.SUCCEEDED];
+    const appliedFilters = [
+      { key: 'phase', operation: 'value_in', value: ['FAILED', 'SUCCEEDED'] },
+    ];
+    mockUseNodeExecutionFiltersState.mockReturnValue({
+      filters: [],
+      appliedFilters,
+    });
+
+    const nodeExecutionsById = mockExecutionsById(2, phases);
+    const filteredNodeExecutions: NodeExecution[] = Object.values(nodeExecutionsById);
+    listNodeExecutions.mockImplementation(() => {
+      return Promise.resolve({
+        entities: filteredNodeExecutions,
+      });
+    });
 
     const { queryAllByTestId } = renderTable({
       initialNodes,
       nodeExecutionsById,
-      filteredNodes,
     });
 
-    await waitFor(() => queryAllByTestId('node-execution-row'));
+    await waitFor(() =>
+      expect(listNodeExecutions).toHaveBeenCalledWith(
+        expect.objectContaining(executionId),
+        expect.objectContaining({
+          filter: appliedFilters,
+        }),
+      ),
+    );
+
+    await waitFor(() => {
+      const rows = queryAllByTestId('node-execution-row');
+      return rows.length === filteredNodeExecutions.length;
+    });
 
     expect(queryAllByTestId('node-execution-row')).toHaveLength(
-      filteredNodes.length,
+      filteredNodeExecutions.length,
     );
+
     const ids = queryAllByTestId('node-execution-col-id');
-    expect(ids).toHaveLength(filteredNodes.length);
+    expect(ids).toHaveLength(filteredNodeExecutions.length);
     const renderedPhases = queryAllByTestId('node-execution-col-phase');
-    expect(renderedPhases).toHaveLength(filteredNodes.length);
-    for (const i in filteredNodes) {
-      expect(ids[i]).toHaveTextContent(filteredNodes[i].id);
+    expect(renderedPhases).toHaveLength(filteredNodeExecutions.length);
+
+    for (const i in filteredNodeExecutions) {
+      expect(ids[i]).toHaveTextContent(filteredNodeExecutions[i].id?.nodeId);
       expect(renderedPhases[i]).toHaveTextContent(phases[i].toString());
     }
   });
