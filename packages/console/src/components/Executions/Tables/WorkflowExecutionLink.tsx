a191e04f62792f344dcd871f6b90eddbaf52945b
Frank Flitton <fflitton@gmail.com>
2023-05-17 10:05:15 -0700

    change style of react import
    Signed-off-by: Frank Flitton <fflitton@gmail.com>

--- packages/console/src/components/Executions/Tables/WorkflowExecutionLink.tsx
+++ packages/console/src/components/Executions/Tables/WorkflowExecutionLink.tsx
@@ -1,4 +1,4 @@
-import * as React from 'react';
+import React from 'react';
 import classnames from 'classnames';
 import { useCommonStyles } from 'components/common/styles';
 import { WorkflowExecutionIdentifier } from 'models/Execution/types';
