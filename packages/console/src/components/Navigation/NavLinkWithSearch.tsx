afc77e88def09cbf8645cd6464e24aa0403bd168
Soham <4nalog@protonmail.com>
2023-05-25 01:50:20 -0400

    fix: preserve domain when navigating using sidebar

---
+++ packages/console/src/components/Navigation/NavLinkWithSearch.tsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import { NavLink, NavLinkProps, useLocation } from 'react-router-dom';
+
+interface NavLinkWithSearchProps extends NavLinkProps {
+  preserve?: string[];
+}
+
+/**
+ * A NavLink that preserves the search params from the current location.
+ *
+ * @param preserve - An array of search param keys to preserve. If not specified, all search params will be preserved.
+ */
+export default function NavLinkWithSearch({
+  preserve,
+  ...props
+}: NavLinkWithSearchProps) {
+  const location = useLocation();
+
+  const searchParams = new URLSearchParams(location.search);
+
+  if (preserve && preserve.length) {
+    for (const key of searchParams.keys()) {
+      if (key in preserve) {
+        continue;
+      }
+
+      searchParams.delete(key);
+    }
+  }
+
+  const to = props.to + '?' + searchParams.toString();
+  return <NavLink {...props} to={to} />;
+}
