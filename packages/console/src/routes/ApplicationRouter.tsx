ba870cbfe0ed0f40efb1c52f95cfd075ecb73ddc
Frank Flitton <fflitton@gmail.com>
2023-05-26 14:42:14 -0700

    chore: scaffolding
    Signed-off-by: Frank Flitton <fflitton@gmail.com>

--- packages/console/src/routes/ApplicationRouter.tsx
+++ packages/console/src/routes/ApplicationRouter.tsx
@@ -82,6 +82,7 @@ export const ApplicationRouter: React.FC = () => {
         exact
         path={Routes.WorkflowDetails.path}
         component={withSideNavigation(components.workflowDetails)}
+        // component={components.workflowDetails}
       />
       <Route
         path={Routes.EntityVersionDetails.path}
