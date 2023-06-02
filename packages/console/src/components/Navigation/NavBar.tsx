111c3f05738d5285b5760793d3bb0c659e9fbb63
Frank Flitton <fflitton@gmail.com>
2023-06-01 20:16:51 -0700

    chore: pass layout context to registered components
    Signed-off-by: Frank Flitton <fflitton@gmail.com>

--- packages/console/src/components/Navigation/NavBar.tsx
+++ packages/console/src/components/Navigation/NavBar.tsx
@@ -8,6 +8,7 @@ import { useExternalConfigurationContext } from 'basics/ExternalConfigurationPro
 import { makeStyles } from '@material-ui/core';
 import { CSSProperties } from '@material-ui/core/styles/withStyles';
 import { getFlyteNavigationData } from './utils';
+import { useTopLevelLayoutContext } from './TopLevelLayoutState';
 
 export interface NavBarProps {
   useCustomContent?: boolean;
@@ -19,6 +20,7 @@ const DefaultAppBarContent = lazy(() => import('./DefaultAppBarContent'));
 /** Contains all content in the top navbar of the application. */
 export const NavBar = (props: NavBarProps) => {
   const navData = props.navigationData ?? getFlyteNavigationData();
+  const layoutState = useTopLevelLayoutContext();
 
   const styles = makeStyles(theme => ({
     spacer: theme.mixins.toolbar as CSSProperties,
@@ -49,7 +51,7 @@ export const NavBar = (props: NavBarProps) => {
   const ExternalNav = registry?.nav;
 
   return ExternalNav ? (
-    <ExternalNav />
+    <ExternalNav {...layoutState} />
   ) : (
     <>
       <div className={styles.spacer} />
--- packages/console/src/components/Navigation/index.ts
+++ packages/console/src/components/Navigation/index.ts
@@ -2,9 +2,19 @@ import TopLevelLayout, {
   TopLevelLayoutGrid,
   TopLevelLayoutInterFace,
 } from './TopLevelLayout';
+import TopLevelLayoutProvider, {
+  TopLevelLayoutContext,
+  useTopLevelLayoutContext,
+} from './TopLevelLayoutState';
 
 export * from './UserInformation';
 export * from './NavBarContent';
 export * from './SubNavBarContent';
-export { TopLevelLayout, TopLevelLayoutGrid };
+export {
+  TopLevelLayout,
+  TopLevelLayoutGrid,
+  TopLevelLayoutContext,
+  useTopLevelLayoutContext,
+  TopLevelLayoutProvider,
+};
 export type { TopLevelLayoutInterFace };
