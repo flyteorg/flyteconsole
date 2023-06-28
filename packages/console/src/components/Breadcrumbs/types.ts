98b76332d14be242bc4c3ef6bf6c80867f1113c7
Frank Flitton <fflitton@gmail.com>
2023-06-28 11:51:34 -0700

    chore: add notes to types
    Signed-off-by: Frank Flitton <fflitton@gmail.com>

--- packages/console/src/components/Breadcrumbs/types.ts
+++ packages/console/src/components/Breadcrumbs/types.ts
@@ -1,3 +1,7 @@
+/**
+ * Used for defining the breadcrumbs.
+ * This is used for the breadcrumb registry.
+ */
 export interface Breadcrumb {
   id: string;
   label: string;
@@ -12,15 +16,16 @@ export interface Breadcrumb {
   required: boolean;
 }
 
+/**
+ * The function used for controlling if a breadcrumb should be rednered or not.
+ */
 export type BreadcrumbValidator = (
   valididator: BreadcrumbValidatorInterface,
 ) => boolean;
 
-export type BreadcrumbCustomDefaultValue = (
-  location: Location,
-  breadcrumb: Breadcrumb,
-) => string;
-
+/**
+ * The props used for controlling if a breadcrumb should be rednered or not.
+ */
 export interface BreadcrumbValidatorInterface {
   targetBreadcrumbId: string;
   currentPathSegment: string;
@@ -30,12 +35,27 @@ export interface BreadcrumbValidatorInterface {
   url: string;
 }
 
+/**
+ * The function used for defining the default value of a breadcrumb at runtime.
+ */
+export type BreadcrumbCustomDefaultValue = (
+  location: Location,
+  breadcrumb: Breadcrumb,
+) => string;
+
+/**
+ * interactive breadcrumb popover list item
+ */
 export interface BreadcrumbEntity {
   url: string;
   title: string;
   createdAt: string;
 }
 
+/**
+ * The link to view all of a given collection of breadcrumb entities.
+ * This can be a string or a function that returns a string at runtime.
+ */
 export type BreadcrumbEntityViewAllLink =
   | string
   | ((projectId: string, domainId: string, location: Location) => string);
@@ -47,6 +67,9 @@ export interface BreadcrumbPopoverInterface
   onClose: () => void;
 }
 
+/**
+ * The props used for rendering the breadcrumb list UI components.
+ */
 export interface BreadcrumbFormControlInterface extends Breadcrumb {
   value: string;
   key: string;
